fbbd29a3f7e8e10666ff0bee1f4cd8353c159197
Paul Rosenzweig <me@paulrosenzweig.com>
2013-02-05 22:56:59 -0600

    scp over files before taking down server

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,12 @@
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GOPATH=`pwd` go build -o snake src/snake/main/main.go
 coffee --compile static
 cd ..
+scp snake/snake root@hipstersnake.com:/tmp
+scp snake/index.html root@hipstersnake.com:/tmp
+scp -r snake/static/ root@hipstersnake.com:/tmp
 ssh root@hipstersnake.com 'svc -d /etc/service/snake/'
-scp -r snake root@hipstersnake.com:/root
+ssh root@hipstersnake.com 'cat ~/snake/log.txt >> ~/full_log.txt'
+ssh root@hipstersnake.com 'cp /tmp/snake ~/snake/snake'
+ssh root@hipstersnake.com 'cp /tmp/index.html ~/snake/index.html'
+ssh root@hipstersnake.com 'rm -rf ~/snake/static; cp -r /tmp/static/ ~/snake/'
 ssh root@hipstersnake.com 'svc -u /etc/service/snake/'
